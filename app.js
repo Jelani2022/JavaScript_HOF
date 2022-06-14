e9e37faf00ac1d0cbc1053b5b7fb5f2f4682b4a5
Jelani2022 <jtwash2@gmail.com>
2022-06-14 14:50:17 -0400

    submit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 console.log("Hello World!\n==========\n");
 function plus(number){
-    return function plusNumber(number){
-        let plus15 = plus(15)
+    return function (plusNumber){
+        return number + plusNumber
     }
 }
+let plus15 = plus(15)
 console.log(plus(10))
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
@@ -34,3 +35,28 @@ let users = [
     },
   ];
   console.log(users)
+
+  users.forEach(user => console.log(user.name)) 
+
+  // Exercise 3: map
+  let newUsers = users.map((user) => {
+    return {name: user.name, score: user.score};
+  });
+
+  console.log(newUsers);
+
+
+  // Exercise 4: filter
+  let activeUsers = users.filter((user) => user.isActive);
+
+  console.log(activeUsers)
+
+
+  // Exercise 5: sort
+  users.sort((a,b) => b.name - a.name);
+console.log(users)
+  // Exercise 6: reduce
+
+  let avgScore = users.reduce((sum,user ) => sum + user.score, 0) / user.length;
+  
+  console.log(avgScore)
